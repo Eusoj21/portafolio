368fcd898f835445e030278d2cb331c1d7a1194b
Eusoj21 <clarens2015cedral@gmail.com>
2026-01-19 10:36:31 -0600

    Cambios ultimos

--- Proyectos de desarrollo/portafolio-main/next.config.mjs
+++ Proyectos de desarrollo/portafolio-main/next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Esto ayuda a que Vercel maneje mejor las rutas si hay dudas
+  trailingSlash: true,
+  // Esto asegura que las imágenes carguen sin problemas
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
